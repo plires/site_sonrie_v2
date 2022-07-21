8b22adc2996b19aae79c7c30e32350caff94bb53
Pablo Lires <pablolires@gmail.com>
2022-07-21 15:15:44 -0300

    Eliminacion de script en index.php

--- index.php
+++ index.php
@@ -223,7 +223,6 @@
 	<?php include_once('./includes/footer.php'); ?>
 
 	<script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
-	<script type="text/javascript" src="./node_modules/jquery-easing/dist/jquery.easing.1.3.umd.min.js"></script>
 	<script src="https://www.google.com/recaptcha/api.js?render=<?= RECAPTCHA_KEY_SITE ?>"></script>
 	<script type="text/javascript" src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="./node_modules/aos/dist/aos.js"></script>
