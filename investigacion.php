dde5aa91b3c1d0ee6a3f68867a16c6f0273e20fe
Pablo Lires <pablolires@gmail.com>
2022-07-25 11:54:59 -0300

    Agregado de seccion investigacion

---
+++ investigacion.php
@@ -0,0 +1,471 @@
+<?php
+	
+	require ('includes/config.inc.php');
+  include_once('includes/soporte.php');
+
+  $current = 'investigacion';
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<!-- Tag Manager Head -->
+	<?php include_once("./includes/tag_manager_head.php"); ?>
+	
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="nos enfocamos en las afecciones de Grado 2 del Bienestar, la falta de motivación, insomnio, depresión, traumas, bullying, ansiedad y violencia">
+	<title>Investigación & Desarrollo - Sonrie la Vida</title>
+
+	<!-- Favicons -->
+	<?php include('includes/favicon.php'); ?>
+
+	<link rel="stylesheet" type="text/css" href="./node_modules/normalize.css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./node_modules/aos/dist/aos.css"/>
+	<link rel="stylesheet" href="./css/app.css">
+</head>
+<body>
+	<!-- Tag Manager Body -->
+	<?php include_once("./includes/tag_manager_body.php"); ?>
+
+	<!-- Contenido INVESTIGACION -->
+	<section id="app" class="transition investigacion">
+
+		<!-- Header -->
+		<?php include_once('./includes/header.php'); ?>
+
+		<!-- Pos Header -->
+		<section id="first_section" class="container-fluid pos_header">
+			<p class="rinascere">Rinascere</p>
+		</section>
+		<!-- Pos Header end -->
+
+		<!-- Centro de Investigación -->
+		<section data-aos="fade-up" class="container centro_investigacion">
+			<div class="row">
+
+				<div class="col-md-10 offset-md-1">
+
+					<img class="img-fluid img_centro" src="./img/investigacion/icono-centro.png" alt="icono centro de investigacion">
+					<p class="centro_de">Centro de</p>
+					<h1>Investigación & Desarrollo</h1>
+					<h3>"No hay enseñanza sin investigación, ni investigación sin enseñanza" <strong>Pablo Freire</strong></h3>
+
+					<div class="row data">
+						<div class="col-md-6">
+							<p>
+								Una vez decidido nuestro rol y la forma de cooperar creamos nuestro Centro de Investigación y Desarrollo y comenzamos a llevar a cabo diversos trabajos científicos uniendo la IA y la medicina de precisión y nos enfocamos en las afecciones de Grado 2 del Bienestar, la falta de motivación, insomnio, depresión, traumas, bullying, ansiedad, violencia, etc. que no permiten el desarrollo de las personas y así potenciar al máximo sus dotes y talentos
+							</p>
+						</div>	
+
+						<div class="col-md-6">
+							<p>
+								Desde esta área pretendemos investigar y desarrollar trabajos y tecnologías que permitan asistir a individuos como a distintas organizaciones sin fines de lucro; buscamos proporcionarles herramientas capaces de ayudar a contribuir al avance de las personas en sus Grados del Bienestar, el programa reunió padrinos, laboratorios, empresas privadas, obras sociales, gobiernos, etc.
+							</p>
+						</div>	
+					</div>
+
+				</div>
+
+			</div>
+		</section>
+		<!-- Centro de Investigación end -->
+
+		<!-- Un Logro -->
+		<section class="container-fluid p-0 un_logro">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-12 ojo">
+						<img class="img-fluid" src="./img/investigacion/ojo.jpg" alt="ojo investigacion">
+					</div>
+
+					<div class="data">
+						<p data-aos="fade-up"  class="outline">Un logro muy</p>
+						<p data-aos="fade-up"  class="logro_importante">Nuestro más importante logro <br>en estos años de investigación fue:</p>
+						<p data-aos="fade-up"  class="creacion">"La creación del PROGRAMA RENASCERE"</p>
+						<p data-aos="fade-up"  class="outline">"importante"</p>
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<!-- Un Logro end -->
+
+		<!-- Caracteristicas Rinascere -->
+		<section class="container-fluid p-0 caracteristicas">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-10 offset-md-1">
+
+						<div class="row">
+
+							<div data-aos="fade-up"  class="col-md-4 palabra">
+								<img class="img-fluid" src="./img/investigacion/rinascere-vertical.png" alt="rinascere vertical">
+							</div>
+
+							<div class="col-md-8 info">
+
+								<div class="colibri_black">
+									<img data-aos="fade-up"  class="img-fluid" src="./img/investigacion/colibri-black.png" alt="colibri black">
+								</div>
+
+								<div class="content content_first">
+									<div class="title title_number">
+									</div>
+									<div class="description">
+									</div>
+								</div>
+
+								<div data-aos="fade-up"  class="content">
+									<div class="title">
+										QUÉ<br>ES?
+									</div>
+									<div class="description">
+										<p>
+											Rinascere, es un método de intervención de procesos que incluyen, la biología, la física aplicada, la genética, el estudio de los procesos cerebrales, y el balance emocional.
+										</p>
+									</div>
+								</div>
+
+								<div data-aos="fade-up"  class="content">
+									<div class="title">
+										QUÉ<br>HACE?
+									</div>
+									<div class="description">
+										<p>
+											Para ello no solo nos basamos en áreas como la genética, exigenética sino también aportamos tecnología de Inteligencia Artificial y Bioinformática aplicada para desarrollar protocolos de estudios y valoración diagnóstica/estadística.
+										</p>
+									</div>
+								</div>
+
+								<div data-aos="fade-up"  class="content">
+									<div class="title">
+										CON QUÉ<br>OBJETIVO?
+									</div>
+									<div class="description">
+										<p>
+											El objetivo del método es "entender" las causas que subyacen a partir de la aparición de síntomas o signos de desequilibrio que llevan en muchos casos a un gran número de enfermedades, apuntamos precisamente a entender cómo los procesos de envejecimiento actúan en todas estas áreas y así podemos intervenir a tiempo brindando al paciente información, la cual, es fundamental para concientizar y organizar estrategias basadas en prevención e intervención temprana. 
+										</p>
+									</div>
+								</div>
+
+								<div data-aos="fade-up"  class="central">
+									<div class="img_cerebro">
+										<img class="img-fluid" src="./img/investigacion/icono-genetica-small.png" alt="cerebro">
+									</div>
+									<div class="frase">
+										<img class="img-fluid" src="./img/investigacion/frase_genetica.png" alt="frase genetica">
+									</div>
+								</div>
+
+								<div class="content content_first">
+									<div class="title">
+									</div>
+									<div class="description">
+									</div>
+								</div>
+
+								<div data-aos="fade-up"  class="content">
+									<div class="title title_number title_number">
+										01
+									</div>
+									<div class="description">
+										<p>
+											Nuestra <strong>medicina personalizada</strong> consiste en utilizar el <strong>perfil genético</strong> de un individuo; el cual sumado a tecnologías de IA, que nos brindan perfiles emocionales y neurocognitivos, asociados a cambios de hábitos alimentarios, y al aporte de nutriceuticos logramos así <strong>"entender que cada uno es único e irrepetible"</strong>
+										</p>
+									</div>
+								</div>
+
+								<div data-aos="fade-up"  class="content content_last">
+									<div class="title title_number">
+										02
+									</div>
+									<div class="description">
+										<p>
+											Con eso nuestros terapeutas pueden guiar a los pacientes en relación a programas de diagnóstico, prevención e intervención en las causas que conllevan al envejecimiento. Aspirando a que cada uno de nuestros pacientes logre alcanzar el Bienestar.
+										</p>
+									</div>
+								</div>
+
+							</div>
+
+						</div>
+					
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<!-- Caracteristicas Rinascere end -->
+
+		<!-- Método -->
+		<section class="container-fluid metodo">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-12">
+						<h2 data-aos="fade-up">Método explicado paso a paso</h2>
+						<p data-aos="fade-up" class="frase">
+							Inteligencia Artificial puesta a disposición del Médico, mayor tiempo para con el paciente. Mejora el trato humano, Terapias de Bienestar y no invasivas.
+						</p>
+					</div>
+
+					<div class="col-lg-6">
+						<h3 data-aos="fade-up">MECANISMO DE PROCESOS</h3>
+						<img data-aos="fade-up" class="img-fluid img_metodo" src="./img/investigacion/mecanismos.png" alt="mecanismos sonrie la vida">
+					</div>
+
+					<div class="col-lg-6">
+						<h3 data-aos="fade-up">ÁREAS DE INTERACCIÓN</h3>
+						<img data-aos="fade-up" class="img-fluid img_areas" src="./img/investigacion/areas.png" alt="areas sonrie la vida">
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<!-- Método end -->
+
+		<!-- Noticias -->
+		<section class="container-fluid noticias">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-12">
+						<h2 data-aos="fade-up" >Noticias</h2>
+						<p data-aos="fade-up"  class="subtitulo">
+							Acá te mostramos como aplicamos nuestra tecnología para la asistencia social
+						</p>
+					</div>
+
+					<div data-aos="fade-up" class="col-md-4">
+						<div class="content">
+							<div>
+								<a
+									target="_blank" 
+									rel="noopener" 
+									href="https://tn.com.ar/salud/noticias/2020/12/23/coronavirus-un-sistema-de-inteligencia-artificial-detecta-el-covid-19-por-la-voz/">
+									<img class="img-fluid transition" src="./img/investigacion/noticia-1.jpg" alt="noticias 1 sonrie la vida">
+								</a>
+								<h3>Aquera RSI: un software que detecta covid-19 por voz.</h3>
+								<p class="description">
+									Inteligencia artificial que detecta las micro frecuencias que presentan en la voz las personas infectadas.
+								</p>
+							</div>
+							<a
+								target="_blank" 
+								rel="noopener" 
+								href="https://tn.com.ar/salud/noticias/2020/12/23/coronavirus-un-sistema-de-inteligencia-artificial-detecta-el-covid-19-por-la-voz/" 
+								class="btn btn-primary w-100 transition">ver más
+							</a>
+						</div>
+					</div>
+
+					<div data-aos="fade-up" class="col-md-4">
+						<div class="content">
+							<div>
+								<a
+									target="_blank" 
+									rel="noopener" 
+									href="https://www.iprofesional.com/tecnologia/333454-inteligencia-artificial-ya-detecta-coronavirus-por-la-voz">
+									<img class="img-fluid transition" src="./img/investigacion/noticia-2.jpg" alt="noticias 2 sonrie la vida">
+								</a>
+								<h3>Inteligencia artificial y covid-19.</h3>
+								<p class="description">
+									Un nuevo método más eficaz en la identificación temprana de individuos asintomáticos.
+								</p>
+							</div>
+							<a
+								target="_blank" 
+								rel="noopener" 
+								href="https://www.iprofesional.com/tecnologia/333454-inteligencia-artificial-ya-detecta-coronavirus-por-la-voz" 
+								class="btn btn-primary w-100 transition">ver más
+							</a>
+						</div>
+					</div>
+
+					<div data-aos="fade-up" class="col-md-4">
+						<div class="content">
+							<div>
+								<a
+									target="_blank" 
+									rel="noopener" 
+									href="https://ia-latam.com/2021/02/18/llega-a-la-argentina-una-ia-para-detectar-coronavirus-por-la-voz/">
+									<img class="img-fluid transition" src="./img/investigacion/noticia-3.jpg" alt="noticias 3 sonrie la vida">
+								</a>
+								<h3>Llega a la Argentina un software para detectar coronavirus por vos.</h3>
+								<p class="description">
+									El desarrollo de la aplicación y el testeo en un hospital de Suipacha. 
+								</p>
+							</div>
+							<a
+								target="_blank" 
+								rel="noopener" 
+								href="https://ia-latam.com/2021/02/18/llega-a-la-argentina-una-ia-para-detectar-coronavirus-por-la-voz/" 
+								class="btn btn-primary w-100 transition">ver más
+							</a>
+						</div>
+					</div>
+
+					<div data-aos="fade-up" class="col-md-4">
+						<div class="content">
+							<div>
+								<a
+									target="_blank" 
+									rel="noopener" 
+									href="https://www.eldiariodecarlospaz.com.ar/sociedad/2021/2/16/prueban-en-argentina-un-sistema-para-detectar-coronavirus-por-la-voz-109194.html">
+									<img class="img-fluid transition" src="./img/investigacion/noticia-4.jpg" alt="noticias 4 sonrie la vida">
+								</a>
+								<h3>Una apuesta a las tecnologías de la información.</h3>
+								<p class="description">
+									Una aplicación que detecta covid-19 en 15 minutos. 
+								</p>
+							</div>
+							<a
+								target="_blank" 
+								rel="noopener" 
+								href="https://www.eldiariodecarlospaz.com.ar/sociedad/2021/2/16/prueban-en-argentina-un-sistema-para-detectar-coronavirus-por-la-voz-109194.html" 
+								class="btn btn-primary w-100 transition">ver más
+							</a>
+						</div>
+					</div>
+
+					<div data-aos="fade-up" class="col-md-4">
+						<div class="content">
+							<div>
+								<a
+									target="_blank" 
+									rel="noopener" 
+									href="https://www.el1digital.com.ar/sociedad/esta-en-fase-de-prueba-un-test-para-covid-19-que-se-realiza-a-traves-de-la-voz/">
+									<img class="img-fluid transition" src="./img/investigacion/noticia-5.jpg" alt="noticias 5 sonrie la vida">
+								</a>
+								<h3>Se encuentra en fase de prueba un nuevo método de detección de covid-19.</h3>
+								<p class="description">
+									Detectá covid a través de la voz. 
+								</p>
+							</div>
+							<a
+								target="_blank" 
+								rel="noopener" 
+								href="https://www.el1digital.com.ar/sociedad/esta-en-fase-de-prueba-un-test-para-covid-19-que-se-realiza-a-traves-de-la-voz/" 
+								class="btn btn-primary w-100 transition">ver más
+							</a>
+						</div>
+					</div>
+
+					<div data-aos="fade-up" class="col-md-4">
+						<div class="content">
+							<div>
+								<a
+									target="_blank" 
+									rel="noopener" 
+									href="https://tn.com.ar/salud/2021/03/08/un-nuevo-metodo-de-inteligencia-artificial-permite-la-deteccion-del-coronavirus-por-voz/">
+									<img class="img-fluid transition" src="./img/investigacion/noticia-6.jpg" alt="noticias 6 sonrie la vida">
+								</a>
+								<h3>Un sistema innovador para la detección del coronavirus.</h3>
+								<p class="description">
+									La Fundación Sonríe La Vida trae al país Aquera RSI.
+								</p>
+							</div>
+							<a
+								target="_blank" 
+								rel="noopener" 
+								href="https://tn.com.ar/salud/2021/03/08/un-nuevo-metodo-de-inteligencia-artificial-permite-la-deteccion-del-coronavirus-por-voz/" 
+								class="btn btn-primary w-100 transition">ver más
+							</a>
+						</div>
+					</div>
+
+					<div data-aos="fade-up" class="col-md-4">
+						<div class="content">
+							<div>
+								<a
+									target="_blank" 
+									rel="noopener" 
+									href="https://noticiasmercedinas.com/site/2020/12/24/suipacha-hace-punta-detectan-covid-19-a-traves-de-la-voz/">
+									<img class="img-fluid transition" src="./img/investigacion/noticia-7.jpg" alt="noticias 7 sonrie la vida">
+								</a>
+								<h3>Detectan 98 casos de covid en Suipacha.</h3>
+								<p class="description">
+									Se probó en Suipacha una nueva propuesta tecnológica de detección del covid-19.
+								</p>
+							</div>
+							<a
+								target="_blank" 
+								rel="noopener" 
+								href="https://noticiasmercedinas.com/site/2020/12/24/suipacha-hace-punta-detectan-covid-19-a-traves-de-la-voz/" 
+								class="btn btn-primary w-100 transition">ver más
+							</a>
+						</div>
+					</div>
+
+					<div data-aos="fade-up" class="col-md-4">
+						<div class="content">
+							<div>
+								<a
+									target="_blank" 
+									rel="noopener" 
+									href="https://bahiacesar.com/2021/03/01/inteligencia-artificial-detecta-coronavirus-por-la-voz/">
+									<img class="img-fluid transition" src="./img/investigacion/noticia-8.jpg" alt="noticias 8 sonrie la vida">
+								</a>
+								<h3>Detección de coronavirus: el método con resonancia protónica</h3>
+								<p class="description">
+									Un software de AI llegó a la Argentina.
+								</p>
+							</div>
+							<a
+								target="_blank" 
+								rel="noopener" 
+								href="https://bahiacesar.com/2021/03/01/inteligencia-artificial-detecta-coronavirus-por-la-voz/" 
+								class="btn btn-primary w-100 transition">ver más
+							</a>
+						</div>
+					</div>
+
+					<div data-aos="fade-up" class="col-md-4">
+						<div class="content">
+							<div>
+								<a
+									target="_blank" 
+									rel="noopener" 
+									href="https://chubutline.com/ya-aplican-la-inteligencia-artificial-que-detecta-coronavirus-por-la-voz/">
+									<img class="img-fluid transition" src="./img/investigacion/noticia-9.jpg" alt="noticias 9 sonrie la vida">
+								</a>
+								<h3>Nuevo test de covid-19 a través de la voz.</h3>
+								<p class="description">
+									Ayuda a evitar la propagación gracias a su detección temprana del virus.
+								</p>
+							</div>
+							<a
+								target="_blank" 
+								rel="noopener" 
+								href="https://chubutline.com/ya-aplican-la-inteligencia-artificial-que-detecta-coronavirus-por-la-voz/" 
+								class="btn btn-primary w-100 transition">ver más
+							</a>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<!-- Noticias end -->
+
+	</section>
+	<!-- Contenido INVESTIGACION end -->
+
+	<!-- Footer -->
+	<?php include_once('./includes/footer.php'); ?>
+
+	<script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="https://www.google.com/recaptcha/api.js?render=<?= RECAPTCHA_KEY_SITE ?>"></script>
+	<script type="text/javascript" src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="./node_modules/aos/dist/aos.js"></script>
+	<script type="text/javascript" src="./js/app.js"></script>
+
+</body>
+
+</html>
