a48c00473360f49e458ec6a35e403f6ccf6652cf
Pablo Lires <pablolires@gmail.com>
2022-12-16 14:18:05 -0300

    cambio de estilos, textos y logo en page campaign-fiel-companero.php

--- campaign-fiel-companero.php
+++ campaign-fiel-companero.php
@@ -53,17 +53,21 @@
 					<div data-aos="fade-up" class="col-md-6 content_data">
 
 						<div class="content_frase">
-
+							
 							<p data-aos="fade-up">
-								<span>Para estos Reyes Magos queremos regalarle sonrisas a los chicos que se encuentran en Cuidados Paliativos del Hospital General de Niños R. Gutiérrez dándoles un muñeco Big Boy personalizado! </span>
+								<span>Para estos Reyes Magos queremos regalarle sonrisas a los chicos que se encuentran internados en el Hospital General de Niños R. Gutiérrez dándoles un muñeco Big Boy personalizado!</span>
 							</p>
 
 							<p data-aos="fade-up">
-								Buscamos una forma distinta de recaudar fondos para la campaña y nos propusimos difundir y vender obras de arte físicas y digitales donde lo recaudado se destinará  a la producción de los muñecos junto a un kit de regalo. 
+								Buscamos una forma distinta de recaudar fondos para la campaña y nos propusimos difundir y vender obras de arte físicas y digitales donde lo recaudado se destinará a la producción de los muñecos junto a un kit de regalo.
 							</p>
 
 							<p data-aos="fade-up">
 								Con la colaboración y compromiso de 10 artistas argentinos que se sumaron creando cuadros de Big Boys y Fyre Community Studio que nos donó NFTs, logramos crear una colección de más de 100 piezas únicas.
+							</p>
+
+							<p data-aos="fade-up">
+								Junto a la Cooperadora del Hospital General de Niños R. Gutiérrez queremos contagiar sonrisas y regalarle unos Reyes Magos diferentes a los chicos!
 							</p>
 
 							<p data-aos="fade-up">
@@ -72,7 +76,8 @@
 
 							<p data-aos="fade-up">
 								<strong>¿Querés colaborar con la campaña? Hacé click en el siguiente botón.</strong>
-							</p>
+							</p>						
+
 							<div class="text-center">
 								<a href="ventas-campaign-fiel-companero.php" class="btn btn-primary">Regalá Sonrisas</a>
 							</div>
@@ -80,7 +85,7 @@
 
 						<div class="content_logos">
 							<img class="img-fluid" src="./img/campaigns/fiel-companero/fire-community.png" alt="logo fire community">
-							<img class="img-fluid featured" src="./img/campaigns/fiel-companero/hospital-gutierrez-sm.png" alt="logo hospital gutierrez">
+							<img class="img-fluid featured" src="./img/campaigns/fiel-companero/logo-coperadora.png" alt="logo cooperadora">
 							<img class="img-fluid" src="./img/campaigns/fiel-companero/logo-3-sm.png" alt="logo 3">
 						</div>
 
